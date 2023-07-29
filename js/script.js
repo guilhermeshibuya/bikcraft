bc70570c0cba173efaf698c439cbcd6a57d762b0
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2023-07-28 22:13:38 -0300

    gallery and questions script

--- js/script.js
+++ js/script.js
@@ -16,3 +16,29 @@ params.forEach((param) => {
   const element = document.getElementById(param);
   if (element) element.checked = true;
 });
+
+// PERGUNTAS FREQUENTES
+const questions = document.querySelectorAll(".questions__list button");
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const controls = question.getAttribute("aria-controls");
+    const answer = document.getElementById(controls);
+
+    question.ariaExpanded === "true"
+      ? (question.ariaExpanded = "false")
+      : (question.ariaExpanded = "true");
+
+    answer.classList.toggle("active");
+  });
+});
+
+// GALERIA DE BICICLETAS
+const gallery = document.querySelectorAll(".bike__imgs img");
+const galleryContainer = document.querySelector(".bike__imgs");
+
+gallery.forEach((img) => {
+  img.addEventListener("click", () => {
+    if (matchMedia("(min-width: 950px)").matches) galleryContainer.prepend(img);
+  });
+});
